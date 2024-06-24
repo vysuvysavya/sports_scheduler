e4da2627ab25c799b8dab583b91a282cee01251f
Your Name <you@example.com>
2024-06-24 19:25:25 +0530

    final commit

--- app.js
+++ app.js
@@ -24,14 +24,6 @@ const formattedDate = (d) => {
   return d.toISOString().split("T")[0];
 };
   
-var dateToday = new Date();
-const today = formattedDate(dateToday);
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-);
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
